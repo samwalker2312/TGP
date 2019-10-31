ae63c365273cf65a43fb5f89153c7fd245f5b2ad
samwalker2312 <samwalker2312@gmail.com>
2019-10-31 14:31:49 +0000

    Fixed so emcee won't get stuck in local minimum, now calculating errors

--- radialvelocity_emcee.py
+++ radialvelocity_emcee.py
@@ -1,8 +1,9 @@
 import emcee
 import numpy as np
 import matplotlib.pyplot as plt
+import corner
 
-class MCradvelminim(object):
+class radvelminimiser_MCMC(object):
 
     def __init__(self, x, y, err):
         self.x = x
@@ -17,7 +18,7 @@ class MCradvelminim(object):
 
     def logprior(self, theta):
         a, b, c = theta
-        if 0 < a < 500 and 0. < b < 5000. and 0 < c < 1000:
+        if 0 < a < 150 and 0 < b < 50 and 0 < c < 2*np.pi:
             return 0.0
         return -np.inf
 
@@ -28,44 +29,65 @@ class MCradvelminim(object):
         return lp + self.loglike(theta)
 
     def run(self):
-        pos = [[50,10,0]] + [[1,1,1]]*np.random.randn(50,3)
+        pos = [[50,1.,np.pi]] + np.random.randn(100,3)
         self.nwalkers, self.ndim = pos.shape
         self.sampler = emcee.EnsembleSampler(self.nwalkers, self.ndim, self.logprob, args = (self.x,self.y,self.err))
         self.sampler.run_mcmc(pos, 100000, progress=True)
 
     def getsamples(self):
-        self.flat_samples = self.sampler.get_chain(discard=200, thin=15, flat=True)
+        self.flat_samples = self.sampler.get_chain(discard=2000, thin=15, flat=True)
 
     def printvals(self):
         for i in range(self.ndim):
             print(np.median(self.flat_samples[:,i]))
 
     def plotcurve(self):
-        xplot = np.linspace(self.x[0],self.x[-1],500)
+        xplot = np.linspace(self.x[0],self.x[-1],5000)
         yplot = np.median(self.flat_samples[:,0])*np.sin(np.median(self.flat_samples[:,1])*xplot + np.median(self.flat_samples[:,2]))
         plt.figure()
         plt.errorbar(self.x,self.y,yerr=self.err, fmt='o')
         plt.plot(xplot, yplot)
+        plt.show()
+        fig = corner.corner(self.flat_samples)
+        plt.show()
+        fig, axes = plt.subplots(3)
+        samples = self.sampler.get_chain()
+        for i in range(self.ndim):
+            ax = axes[i]
+            ax.plot(samples[:, :, i], "k", alpha=0.3)
+            ax.set_xlim(0, len(samples))
+            ax.yaxis.set_label_coords(-0.1, 0.5)
+            axes[-1].set_xlabel("step number")
         plt.show()
 
 def main():
     x = np.loadtxt('radvel.txt',comments='#', usecols=0)
     y =  np.loadtxt('radvel.txt',comments='#', usecols=1)
     err =  np.loadtxt('radvel.txt',comments='#', usecols=2)
-    minimiser = MCradvelminim(x,y,err)
+    minimiser = radvelminimiser_MCMC(x,y,err)
     minimiser.run()
     minimiser.getsamples()
-    #minimiser.printvals()
-    period = 2*np.pi/np.median(minimiser.flat_samples[:,1])
-    print("Period in days is " + str(period))
-    starmass = float(input("Input the mass of the host star: "))
+    minimiser.printvals()
+    b = np.percentile(minimiser.flat_samples[:,1], (16,50,84))
+    bpluserror = b[2] - b[1]
+    bnegerror = b[1] - b[0]
+    b = b[1]
+    print("b = " +str(b))
+    period = 2*np.pi/b
+    periodpluserror = period*bpluserror/b
+    periodnegerror = (period*bnegerror/b)
+    print("Period in days is " + str(period) + " +" + str(periodpluserror) + " " + str(periodnegerror))
+    period *= 24*3600
+    periodpluserror *= 24*3600
+    periodnegerror *= 24*3600
+    starmass = 2.5e30 #float(input("Input the mass of the host star: "))
     G = 6.674e-11
-    planetradius = (starmass*G*period**2./(4.*np.pi**2.))**(1/3)
-    print("Radius in Rj is " + str(planetradius/(69911e3)))
-    planetvel = (G*starmass/planetradius)**.5
+    orbitalradius = (starmass*G*period**2./(4.*np.pi**2.))**(1/3)
+    print("Orbital radius in AU is " + str(orbitalradius/(1.496e11)))
+    planetvel = (G*starmass/orbitalradius)**.5
     #print("Orbital velocity is " + str(planetvel))
     planetmass = starmass*np.median(minimiser.flat_samples[:,0])/planetvel
-    print("Planet mass assuming zero inclination in Mj is " + str(np.abs(planetmass/(1.898e27))))
+    print("Planet mass assuming inc=90 in Mj is " + str(np.abs(planetmass/(1.898e27))))
     minimiser.plotcurve()
 
 main()
